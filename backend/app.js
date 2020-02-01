c3b56aa964ca31738507cddb8fe180df9a146503
Collin Murch <collin@murch.net>
2020-02-01 17:42:18 -0600

    initial backend work

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const url = require('url');
+const fs = require('fs');
+const file = JSON.parse(fs.readFileSync('./password.json', 'utf8'));
+
+
+const pass = encodeURI(file.password);
+
+const uri = `mongodb+srv://collinmurch:${pass}@cluster0-vqk4j.mongodb.net/test?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
