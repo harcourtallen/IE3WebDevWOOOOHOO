e970eeff5dc57f912b0827bb874907cbd7819e14
Collin Murch <collin@murch.net>
2020-02-21 13:13:00 -0600

    initial expressjs work

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,36 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./models/User.js').user;
 const url = require('url');
 const fs = require('fs');
+
 const file = JSON.parse(fs.readFileSync('./password.json', 'utf8'));
-
-
-
 const pass = encodeURI(file.password);
-
 const uri = `mongodb+srv://collinmurch:${pass}@cluster0-vqk4j.mongodb.net/test?retryWrites=true&w=majority`;
 
-console.log(uri);
+const port = process.env.port || 8080;
+const app = express();
+
+const Tom = new user({name: 'Tom', surname: 'Tommert', username: 'tom12',
+    email: 'tomtommert@northwestern.edu'});
+
+app.get('/', (req, res) => {
+    res.send('<b>Hello world</b>');
+
+    Tom.save().then(() => {
+        console.log("saved");
+     });
+});
+
+app.put('/adduser', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
-
-console.log("Testing.")
 
 // Cat.deleteMany({ name: 'Zildjian' }, (err) => {
 //     if(err) console.log(err);
 //     console.log("Successful deletion");
 // });
-
-
-const Tom = new user({name: 'Tom', surname: 'Tommert', username: 'tom12',
-    email: 'tomtommert@northwestern.edu'});
-
-Tom.save(function (err) {
-    if (err) return handleError(err);
-    console.log("saved");
-  });
--- backend/models/User.js
+++ backend/models/User.js
@@ -6,12 +6,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String,
     surname: String,
-    username: {type: String, required: [true, "Please enter a username"], index:true},
-    email: {type: String, required: [true, "Please enter an email address"], index:true},
+    username: {type: String, required: [true, "Please enter a username"]},
+    email: {type: String, required: [true, "Please enter an email address"]},
 }, {timestamps: true});
 
 //userSchema.plugin(uniqueValidator, {message: "is already taken."});
 
 
 const user = mongoose.model('User', userSchema);
+
 module.exports = {user};
